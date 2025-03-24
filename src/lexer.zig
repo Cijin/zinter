8b6bb7abdf9dbf2b380b828688e50bf97990e519
sea gin <cijin@seagin.me>
2025-03-25 06:17:12 +0700

    extend tests for incoming features

--- src/lexer.zig
+++ src/lexer.zig
@@ -23,15 +23,16 @@ const lexer = struct {
     fn next_token(l: *lexer) token.token {
         var t: token.token = undefined;
         t = switch (l.ch) {
-            '=' => token.token{ .token_type = token.TokenType.Assign, .literal = l.ch },
-            ';' => token.token{ .token_type = token.TokenType.Semicolon, .literal = l.ch },
-            ',' => token.token{ .token_type = token.TokenType.Comma, .literal = l.ch },
-            '(' => token.token{ .token_type = token.TokenType.Lparen, .literal = l.ch },
-            ')' => token.token{ .token_type = token.TokenType.Rparen, .literal = l.ch },
-            '{' => token.token{ .token_type = token.TokenType.Lbrace, .literal = l.ch },
-            '}' => token.token{ .token_type = token.TokenType.Rbrace, .literal = l.ch },
-            '+' => token.token{ .token_type = token.TokenType.Plus, .literal = l.ch },
+            '=' => token.token{ .token_type = token.TokenType.Assign, .literal = "=" },
+            ';' => token.token{ .token_type = token.TokenType.Semicolon, .literal = ";" },
+            ',' => token.token{ .token_type = token.TokenType.Comma, .literal = "," },
+            '(' => token.token{ .token_type = token.TokenType.Lparen, .literal = "(" },
+            ')' => token.token{ .token_type = token.TokenType.Rparen, .literal = ")" },
+            '{' => token.token{ .token_type = token.TokenType.Lbrace, .literal = "{" },
+            '}' => token.token{ .token_type = token.TokenType.Rbrace, .literal = "}" },
+            '+' => token.token{ .token_type = token.TokenType.Plus, .literal = "+" },
             '0' => token.token{ .token_type = token.TokenType.Eof, .literal = undefined },
+            // Todo: handle let, ident, ints, return and fn
             else => unreachable,
         };
         l.read_char();
@@ -56,15 +57,15 @@ fn New(allocator: mem.Allocator, input: []const u8) !*lexer {
 
 test "next token method" {
     const input = "=+(){},;";
-    const tests = [_]struct { expected_type: token.TokenType, expected_literal: u8 }{
-        .{ .expected_type = token.TokenType.Assign, .expected_literal = '=' },
-        .{ .expected_type = token.TokenType.Plus, .expected_literal = '+' },
-        .{ .expected_type = token.TokenType.Lparen, .expected_literal = '(' },
-        .{ .expected_type = token.TokenType.Rparen, .expected_literal = ')' },
-        .{ .expected_type = token.TokenType.Lbrace, .expected_literal = '{' },
-        .{ .expected_type = token.TokenType.Rbrace, .expected_literal = '}' },
-        .{ .expected_type = token.TokenType.Comma, .expected_literal = ',' },
-        .{ .expected_type = token.TokenType.Semicolon, .expected_literal = ';' },
+    const tests = [_]struct { expected_type: token.TokenType, expected_literal: []const u8 }{
+        .{ .expected_type = token.TokenType.Assign, .expected_literal = "=" },
+        .{ .expected_type = token.TokenType.Plus, .expected_literal = "+" },
+        .{ .expected_type = token.TokenType.Lparen, .expected_literal = "(" },
+        .{ .expected_type = token.TokenType.Rparen, .expected_literal = ")" },
+        .{ .expected_type = token.TokenType.Lbrace, .expected_literal = "{" },
+        .{ .expected_type = token.TokenType.Rbrace, .expected_literal = "}" },
+        .{ .expected_type = token.TokenType.Comma, .expected_literal = "," },
+        .{ .expected_type = token.TokenType.Semicolon, .expected_literal = ";" },
     };
 
     const l = try New(testing.allocator, input);
@@ -73,6 +74,67 @@ test "next token method" {
         const t = l.next_token();
 
         try expect(tests[i].expected_type == t.token_type);
-        try expect(tests[i].expected_literal == t.literal);
+        try expect(mem.eql(u8, tests[i].expected_literal, t.literal));
+    }
+}
+
+test "next token with source code" {
+    const input =
+        \\ let five = 5;
+        \\ let ten = 10;
+        \\  
+        \\ let add = fn(x, y) {
+        \\  return x + y;
+        \\ };
+        \\
+        \\ let result = add(five, ten);
+    ;
+    const tests = [_]struct { expected_type: token.TokenType, expected_literal: []const u8 }{
+        .{ .expected_type = token.TokenType.Let, .expected_literal = "let" },
+        .{ .expected_type = token.TokenType.Ident, .expected_literal = "five" },
+        .{ .expected_type = token.TokenType.Assign, .expected_literal = "=" },
+        .{ .expected_type = token.TokenType.Int, .expected_literal = "5" },
+        .{ .expected_type = token.TokenType.Semicolon, .expected_literal = ";" },
+        .{ .expected_type = token.TokenType.Let, .expected_literal = "let" },
+        .{ .expected_type = token.TokenType.Ident, .expected_literal = "ten" },
+        .{ .expected_type = token.TokenType.Assign, .expected_literal = "=" },
+        .{ .expected_type = token.TokenType.Int, .expected_literal = "10" },
+        .{ .expected_type = token.TokenType.Semicolon, .expected_literal = ";" },
+        .{ .expected_type = token.TokenType.Let, .expected_literal = "let" },
+        .{ .expected_type = token.TokenType.Ident, .expected_literal = "add" },
+        .{ .expected_type = token.TokenType.Assign, .expected_literal = "=" },
+        .{ .expected_type = token.TokenType.Function, .expected_literal = "fn" },
+        .{ .expected_type = token.TokenType.Lparen, .expected_literal = "(" },
+        .{ .expected_type = token.TokenType.Ident, .expected_literal = "x" },
+        .{ .expected_type = token.TokenType.Comma, .expected_literal = "," },
+        .{ .expected_type = token.TokenType.Ident, .expected_literal = "y" },
+        .{ .expected_type = token.TokenType.Rparen, .expected_literal = ")" },
+        .{ .expected_type = token.TokenType.Lbrace, .expected_literal = "{" },
+        .{ .expected_type = token.TokenType.Return, .expected_literal = "return" },
+        .{ .expected_type = token.TokenType.Ident, .expected_literal = "x" },
+        .{ .expected_type = token.TokenType.Plus, .expected_literal = "+" },
+        .{ .expected_type = token.TokenType.Ident, .expected_literal = "y" },
+        .{ .expected_type = token.TokenType.Semicolon, .expected_literal = ";" },
+        .{ .expected_type = token.TokenType.Rbrace, .expected_literal = "}" },
+        .{ .expected_type = token.TokenType.Semicolon, .expected_literal = ";" },
+        .{ .expected_type = token.TokenType.Let, .expected_literal = "let" },
+        .{ .expected_type = token.TokenType.Ident, .expected_literal = "result" },
+        .{ .expected_type = token.TokenType.Assign, .expected_literal = "=" },
+        .{ .expected_type = token.TokenType.Ident, .expected_literal = "add" },
+        .{ .expected_type = token.TokenType.Lparen, .expected_literal = "(" },
+        .{ .expected_type = token.TokenType.Ident, .expected_literal = "five" },
+        .{ .expected_type = token.TokenType.Comma, .expected_literal = "," },
+        .{ .expected_type = token.TokenType.Ident, .expected_literal = "ten" },
+        .{ .expected_type = token.TokenType.Rparen, .expected_literal = ")" },
+        .{ .expected_type = token.TokenType.Semicolon, .expected_literal = ";" },
+    };
+
+    const l = try New(testing.allocator, input);
+    defer testing.allocator.destroy(l);
+    for (0..tests.len) |i| {
+        const t = l.next_token();
+
+        try expect(tests[i].expected_type == t.token_type);
+        try expect(mem.eql(u8, tests[i].expected_literal, t.literal));
     }
 }
--- src/token.zig
+++ src/token.zig
@@ -11,15 +11,16 @@ pub const TokenType = enum {
     Rparen,
     Lbrace,
     Rbrace,
-    Function,
     Let,
+    Function,
+    Return,
 };
 
 pub const token = struct {
     token_type: TokenType,
-    literal: u8,
+    literal: []const u8,
 };
 
-pub fn new_token(t: TokenType, l: u8) token {
+pub fn new_token(t: TokenType, l: []const u8) token {
     return token{ .token_type = t, .literal = l };
 }
