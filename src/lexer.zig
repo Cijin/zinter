f4dad225515e1b5f962bbd88222995c1e4766da6
sea gin <cijin@seagin.me>
2025-03-25 18:27:43 +0700

    tests failing, check why

--- src/lexer.zig
+++ src/lexer.zig
@@ -1,20 +1,23 @@
 const std = @import("std");
 const mem = std.mem;
+const assert = std.debug.assert;
 const testing = std.testing;
 const expect = testing.expect;
 const token = @import("token.zig");
 
 const lexer = struct {
     input: []const u8,
-    position: usize,
-    read_position: usize,
+    position: i64,
+    read_position: i64,
     ch: u8,
 
     fn read_char(l: *lexer) void {
-        if (l.read_position >= l.input.len) {
+        assert(l.read_position <= l.input.len);
+
+        if (l.read_position == l.input.len) {
             l.ch = 0;
         } else {
-            l.ch = l.input[l.read_position];
+            l.ch = l.input[@intCast(l.read_position)];
         }
         l.position = l.read_position;
         l.read_position += 1;
@@ -31,14 +34,51 @@ const lexer = struct {
             '{' => token.token{ .token_type = token.TokenType.Lbrace, .literal = "{" },
             '}' => token.token{ .token_type = token.TokenType.Rbrace, .literal = "}" },
             '+' => token.token{ .token_type = token.TokenType.Plus, .literal = "+" },
+            // Todo: keep an eye on this, there might be a better way to do this
             '0' => token.token{ .token_type = token.TokenType.Eof, .literal = undefined },
-            // Todo: handle let, ident, ints, return and fn
-            else => unreachable,
+            else => {
+                const start_position: usize = @intCast(l.position);
+                var end_position: usize = 0;
+                if (is_letter(l.ch)) {
+                    l.read_identifier();
+                    end_position = @intCast(l.read_position);
+                    if (token.lookup_keyword(l.input[start_position..end_position])) |keyword_token| {
+                        t = keyword_token;
+                        return t;
+                    }
+                    return token.token{ .token_type = token.TokenType.Ident, .literal = l.input[start_position..end_position] };
+                } else if (is_integer(l.ch)) {
+                    end_position = @intCast(l.read_position);
+                    return token.token{ .token_type = token.TokenType.Int, .literal = l.input[start_position..end_position] };
+                } else {
+                    return token.token{ .token_type = token.TokenType.Illegal, .literal = l.input[start_position..(start_position + 1)] };
+                }
+            },
         };
         l.read_char();
         return t;
     }
+
+    fn read_identifier(l: *lexer) void {
+        while (is_letter(l.ch)) {
+            l.read_char();
+        }
+    }
+
+    fn read_integer(l: *lexer) void {
+        while (is_integer(l.ch)) {
+            l.read_char();
+        }
+    }
 };
+
+fn is_integer(ch: u8) bool {
+    return ch >= '0' and ch <= '9';
+}
+
+fn is_letter(ch: u8) bool {
+    return (ch >= 'a' and ch <= 'z') or (ch >= 'A' and ch <= 'Z') or ch == '_';
+}
 
 fn New(allocator: mem.Allocator, input: []const u8) !*lexer {
     // Todo: use arena allocator, ensure arena.deinit() is called when the
--- src/token.zig
+++ src/token.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const TokenType = enum {
     Illegal,
     Eof,
@@ -23,4 +25,20 @@ pub const token = struct {
 
 pub fn new_token(t: TokenType, l: []const u8) token {
     return token{ .token_type = t, .literal = l };
+}
+
+pub const keywords = [_]struct { key: []const u8, token: token }{
+    .{ .key = "let", .token = token{ .token_type = TokenType.Let, .literal = "let" } },
+    .{ .key = "fn", .token = token{ .token_type = TokenType.Function, .literal = "fn" } },
+    .{ .key = "return", .token = token{ .token_type = TokenType.Return, .literal = "return" } },
+};
+
+pub fn lookup_keyword(k: []const u8) ?token {
+    for (keywords) |keyword| {
+        if (std.mem.eql(u8, k, keyword.key)) {
+            return keyword.token;
+        }
+    }
+
+    return null;
 }
