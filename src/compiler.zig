4a3ed8699b828f2b52167c32384adfa0f7451dd7
sea gin <cijin@seagin.me>
2025-05-22 06:39:29 +0700

    semblance of a compiler is taking shape, wip

--- src/compiler.zig
+++ src/compiler.zig
@@ -20,7 +20,41 @@ const compiler = struct {
     instructions: std.ArrayList(u8),
     constants: std.ArrayList(object.Object),
 
-    fn compile(_: compiler, _: ast.Program) CompilerError!void {}
+    fn compile(self: compiler, node: ast.Node) CompilerError!void {
+        switch (node) {
+            .program => |p| {
+                for (p.statements) |stmt| {
+                    try self.compile(ast.Node{ .statement = stmt });
+                }
+            },
+            .statement => |s| {
+                switch (s) {
+                    .expression_statement => |es| {
+                        try self.compile(ast.Node{ .expression = es.expression });
+                    },
+                    else => unreachable,
+                }
+            },
+            .expression => |e| {
+                switch (e) {
+                    .infix_expression => |in| {
+                        try self.compile(ast.Node{ .expression = in.left });
+
+                        try self.compile(ast.Node{ .expression = in.right });
+                    },
+                    .integer => |int| {
+                        self.constants.append(object.Object{ .integer = object.Integer{ .value = int.value } }) catch unreachable;
+
+                        const idx = self.constants.items.len - 1;
+                        const newInstructions = code.make(code.Opcode.opconstant, &.{idx});
+                        var instr = self.instructions.addManyAsSlice(newInstructions.len);
+                        instr = newInstructions;
+                    },
+                    else => unreachable,
+                }
+            },
+        }
+    }
 
     fn byte_code(self: compiler) ByteCode {
         return ByteCode{
@@ -31,19 +65,13 @@ const compiler = struct {
 };
 
 fn New(allocator: mem.Allocator) compiler {
-    const instructions = std.ArrayList(u8).init(allocator);
-    const constants = std.ArrayList(object.Object).init(allocator);
-
     return compiler{
-        .instructions = instructions,
-        .constants = constants,
+        .instructions = std.ArrayList(u8).init(allocator),
+        .constants = std.ArrayList(object.Object).init(allocator),
     };
 }
 
-// Todo: fix this test
-// 1. Check input hex values, they look totally wrong
-// 2. Check tests for code.make, I don't think it's correct
-test "compiler" {
+test "compiled arithmatic instructions" {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const allocator = arena.allocator();
@@ -57,8 +85,8 @@ test "compiler" {
             .input = "1 + 2;",
             .expectedConstants = &.{ 1, 2 },
             .expectedInstructions = &.{
+                code.make(code.Opcode.opConstant, &.{0}, allocator),
                 code.make(code.Opcode.opConstant, &.{1}, allocator),
-                code.make(code.Opcode.opConstant, &.{2}, allocator),
             },
         },
     };
@@ -68,8 +96,8 @@ test "compiler" {
         const p = try parser.New(allocator, l);
         const program = try p.parse_program();
 
-        const c = New(allocator);
-        try c.compile(program);
+        var c = New(allocator);
+        try c.compile(ast.Node{ .program = program });
         const got = c.byte_code();
 
         var concatted_instr = std.ArrayList(u8).init(allocator);
@@ -79,12 +107,12 @@ test "compiler" {
             }
         }
 
+        try testing.expectEqualSlices(u8, concatted_instr.items, got.instructions);
+
         assert(t.expectedConstants.len == got.constants.len);
         for (got.constants, 0..) |constant, i| {
             const int: object.Integer = constant.integer;
             try testing.expectEqual(t.expectedConstants[i], int.value);
         }
-
-        try testing.expectEqualSlices(u8, concatted_instr.items, got.instructions);
     }
 }
